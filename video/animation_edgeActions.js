e5a8325ba0c897eeeb19dacb4944f25ccef18ca5
Furkan Tektas <tektasfurkan@gmail.com>
2014-04-19 13:05:50 +0300

    Video Revision 2
    Added team picture, qr code moved a little bit higher and replay button
    added.

--- video/animation_edgeActions.js
+++ video/animation_edgeActions.js
@@ -1,40 +1,16 @@
-/***********************
-* Adobe Edge Animate Composition Actions
-*
-* Edit this file with caution, being careful to preserve 
-* function signatures and comments starting with 'Edge' to maintain the 
-* ability to interact with these actions from within Adobe Edge Animate
-*
-***********************/
-(function($, Edge, compId){
-var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonly used Edge classes
 
-   //Edge symbol: 'stage'
-   (function(symbolName) {
-      
-      
-      Symbol.bindElementAction(compId, symbolName, "${_instructionsMainGroup}", "click", function(sym, e) {
-         window.open("http://furkantektas.github.io/g9-rover-space-apps-14/", "_blank");
-
-      });
-      //Edge binding end
-
-      Symbol.bindElementAction(compId, symbolName, "${_instructionsMainGroup}", "mouseover", function(sym, e) {
-         // stop the timeline at the given position (ms or label)
-         sym.stop(14758);
-         
-
-      });
-      //Edge binding end
-
-      Symbol.bindElementAction(compId, symbolName, "${_instructionsMainGroup}", "mouseout", function(sym, e) {
-         sym.play();
-         
-
-      });
-      //Edge binding end
-
-   })("stage");
-   //Edge symbol end:'stage'
-
-})(jQuery, AdobeEdge, "EDGE-3181623");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_instructionsMainGroup}","click",function(sym,e){window.open("http://furkantektas.github.io/g9-rover-space-apps-14/","_blank");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_instructionsMainGroup}","mouseover",function(sym,e){sym.stop(14758);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_instructionsMainGroup}","mouseout",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",32038,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_replay}","click",function(sym,e){sym.play(0);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-3181623");
